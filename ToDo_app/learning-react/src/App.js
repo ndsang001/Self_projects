4ac3d7ee45294db40c67e3caa171df8d9dda674a
ndsang001 <sangdinh2001@gmail.com>
2023-01-25 13:38:09 +0200

    New commit 25 January 2023

---
+++ ToDo_app/learning-react/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import React, { Component } from 'react';
+
+// constructor(props){
+//   super(props);
+//   //state
+//   this.state = {
+//     users: []
+//   }
+// }
+
+// componentWillMount(){
+//   axios('https://api.randomuser.me/?nat=US&results=5').then(response => console.log(response))
+// }
+
+class App extends Component {
+    render() {
+        return ( <
+            div className = "App" > < p > I will be back < /p> </div >
+
+        );
+    }
+}
+
+export default App;
